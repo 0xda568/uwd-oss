15db67aed8aeab0afc566264eb1c3db42469a446
DartVanya <dartvanya@gmail.com>
2024-07-24 11:34:36 +0300

    Fixed bugs on Windows 11, rewritten via IAT hook

--- uwd-oss/dllmain.cpp
+++ uwd-oss/dllmain.cpp
@@ -1,63 +1,91 @@
 #include "pch.h"
-
-#include <stdio.h>
-#include <iostream>
-
-#include <string>
+#include "IatHook.h"
 
 // Proxying all needed exports to explorerframe.dll
 #pragma comment(linker, "/export:DllCanUnloadNow=explorerframe.DllCanUnloadNow")
 #pragma comment(linker, "/export:DllGetClassObject=explorerframe.DllGetClassObject")
 #pragma comment(linker, "/export:DllGetVersion=explorerframe.DllGetVersion")
 
-UCHAR origOpsW[12] = { 0 };
-UCHAR patchOpsW[12] = { 0x48, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xe0 };
-FARPROC origExtTxtOutW;
+// Force 1-byte alignment
+#pragma pack(push, 1)
+struct _HOOK_SCRUCT {
+#ifdef _WIN64
+    const BYTE mov_rax[2] = { 0x48, 0xb8 };
+#else // _WIN64
+    const BYTE mov_eax = 0xb8;
+#endif
+    FARPROC func;
+    const BYTE jmp[2] = { 0xff, 0xe0 };
+} patchOpsW;
+#pragma pack(pop)
 
-const RECT* watermarkRect;
+typedef BOOL(WINAPI* _ExtTextOutW)(HDC, int, int, UINT, const RECT*, LPCWSTR, UINT, const INT*);
+
+PIMAGE_THUNK_DATA iat_entry = NULL;
+_ExtTextOutW origExtTxtOutW = NULL;
 
 // Function ExtTextOutW is being replaced with
 BOOL hookExtTextOutW(HDC hdc, int x, int y, UINT options, const RECT* lprect, LPCWSTR lpString, UINT c, const INT* lpDx) {
+    static WCHAR watermark[3][64];
+    static int n = 0;
 
-    // Copies the original 12 bytes to the address of ExtTextOutW
-    WriteProcessMemory(GetCurrentProcess(), origExtTxtOutW, origOpsW, 12, 0);
+    HWND hwnd = WindowFromDC(hdc);
+    WCHAR className[8];
+    if (hwnd)
+        GetClassNameW(hwnd, className, ARRAYSIZE(className));
 
-    BOOL ret = 0;
-
-    if (!lstrcmpW(lpString, L"Test Mode")) {
-        watermarkRect = lprect;
+    // Shell32 calls ExtTextOutW on explorer start to draw watermark
+    if (n == 0 && !lstrcmpW(lpString, L"Test Mode")) {
+        lstrcpyW(watermark[n++], lpString);
+        return TRUE;
+    }
+    else if (n < 3 && lstrlenW(lpString)) {
+        lstrcpyW(watermark[n++], lpString);
+        return TRUE;
+    }
+    // Menu -> Next desktop wallpaper - hwnd will be NULL.
+    else if ((!hwnd || !lstrcmpW(className, L"WorkerW") || !lstrcmpW(className, L"Progman")) && lstrlenW(lpString)) {
+        for (int i = 0; i < n; i++)
+            if (!lstrcmpW(lpString, watermark[i]))
+                return TRUE;    // remove all types of cached watermark line
     }
 
-    if (lprect == watermarkRect) {
-        ret = true;
-    }
-    else {
-        ret = ExtTextOutW(hdc, x, y, options, lprect, lpString, c, lpDx);
-    }
-
-    // Hooks the function again before exiting
-    WriteProcessMemory(GetCurrentProcess(), origExtTxtOutW, patchOpsW, 12, 0);
-
-    return ret;
+    // Call original ExtTxtOutW function
+    return origExtTxtOutW(hdc, x, y, options, lprect, lpString, c, lpDx);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH: {
 
-        origExtTxtOutW = GetProcAddress(GetModuleHandle(L"gdi32.dll"), "ExtTextOutW");
+        // Searching Shell32 IAT for gdi32.dll!ExtTextOutW
+        HMODULE hShell32 = GetModuleHandle(L"Shell32.dll");
+        if (hShell32)
+            iat_entry = FindIatThunkInModule(hShell32, "gdi32.dll", "ExtTextOutW");
 
-        void* hookPtr = &hookExtTextOutW;
-        memcpy_s(patchOpsW + 2, 8, &hookPtr, 8);
-
-        ReadProcessMemory(GetCurrentProcess(), origExtTxtOutW, origOpsW, 12, 0);
-        WriteProcessMemory(GetCurrentProcess(), origExtTxtOutW, patchOpsW, 12, 0);
+        if (iat_entry) {
+            // Saving original function
+            origExtTxtOutW = (_ExtTextOutW)iat_entry->u1.Function;
+            DWORD oldProt;
+            VirtualProtect(iat_entry, sizeof(iat_entry), PAGE_READWRITE, &oldProt);
+            // Writing address to our hooked function
+            iat_entry->u1.Function = (DWORD_PTR)hookExtTextOutW;
+            VirtualProtect(iat_entry, sizeof(iat_entry), oldProt, &oldProt);
+        }
 
         break;
     }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
+        break;
     case DLL_PROCESS_DETACH:
+        if (iat_entry) {
+            DWORD oldProt;
+            VirtualProtect(iat_entry, sizeof(iat_entry), PAGE_READWRITE, &oldProt);
+            // Unhook function
+            InterlockedCompareExchangePointer((PVOID*)&iat_entry->u1.Function, origExtTxtOutW, hookExtTextOutW);
+            VirtualProtect(iat_entry, sizeof(iat_entry), oldProt, &oldProt);
+        }
         break;
     }
     return TRUE;
